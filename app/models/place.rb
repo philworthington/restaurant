06d6347250f6b4eb759ab002e863961ae061b28b
Phil Worthington <pworthington27@gmail.com>
2014-01-01 23:57:28 -0800

    Build place_spec, add validators to pass

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,3 @@
 class Place < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Place do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+   @place = Place.new(name: "McDonalds")
+  end
+
+  subject { @place }
+
+  it { should respond_to(:name) }
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @place.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is already taken" do
+    before do
+      place_with_same_name = @place.dup
+      place_with_same_name.name = @place.name.upcase
+      place_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
 end
+
+
