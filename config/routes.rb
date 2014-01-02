853076523067b9be92684834a946af610b6e313b
Phil Worthington <pworthington27@gmail.com>
2014-01-02 00:00:15 -0800

    Add static_pages, update root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Restaurant::Application.routes.draw do
+  get "static_pages/index"
   resources :places
+
+  root 'static_pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
