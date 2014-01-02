dbf9b38f038343685edc7e64f2112ce491314e0e
Phil Worthington <pworthington27@gmail.com>
2014-01-01 23:40:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Restaurant
+
+By [Phil Worthington](https://github.com/philworthington).
+
+
+## Description
+AngularJS + Rails 4 running on PosgreSQL.
+
+
+## Authors
+
+* Phil Worthington (https://github.com/philworthington)
+
+
+## License
+
+MIT: http://philworthington.mit-license.org
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restaurant::Application.config.secret_key_base = '9b2f1e9aafe16b3c1bfa6d1c29a5f7cc3aa2bb30a7b54c35f9aadddb0bc5c2b1677bea1184b970985a4fdc5d02cff63d2fb3cf1ca0823e03b90b44d93e19731a'
